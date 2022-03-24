0abb77b35342779d0e94664001221af3a00d3562
klinsman <joseklinsman259@gmail.com>
2022-03-23 23:41:09 -0300

    Atualizando

---
+++ api/index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+const redis = require('./database/redis')
+const mongodb = require('./database/mongo')
+
+
+app.get('/lerAvisos', redis.lerAvisos);
+
+app.post('/salvarAviso', (req, res) => {
+  redis.salvarAvisos(req.body.aviso)
+  res.end("ok");
+})
+
+app.post('/removerAviso', (req, res) => {
+  console.log(req.body.aviso)
+  redis.removerAvisos(req.body.aviso)
+  res.end("ok");
+})
+
+app.get('/getLatLng/:matricula', mongodb.obterLocalizacao);
+
+app.post('/salvarLocalizacao', (req, res) => {
+  console.log(req.body.lat, req.body.lng)
+  let obj = {
+    'matricula': req.body.matricula,
+    'lat': req.body.lat,
+    'lng': req.body.lng
+  }
+  mongodb.salvarLocalizacao(obj)
+  res.end("ok");
+})
+
+app.get('/deleteLocalizacao/:matricula', (req, res) => {
+  console.log(req.params.matricula)
+  mongodb.deleteLocalizacao(req.params.matricula)
+  res.end("ok");
+})
+
+
+app.listen(process.env.PORT, () => { 
+  console.log(`http://localhost:${process.env.PORT}`); 
+});
